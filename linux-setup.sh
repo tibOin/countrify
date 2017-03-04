574f6ef838ac02a41aac50e82a041d73d2ee0571
tibOin <guillaumecharua@iMac-de-Guillaume.local>
2017-03-04 07:57:29 +0100

    improvements

--- linux-setup.sh
+++ linux-setup.sh
@@ -1,13 +1,43 @@
 #!/usr/bin/env bash
 
-echo "Installing Tor\n"
-apt-get install tor
+function echoOrange {
+  echo "$(tput setaf 214)$1$(tput sgr0)"
+}
 
-echo "\nEnsuring pip is up to date\n"
+function echoVert {
+  echo "$(tput setaf 10)$1$(tput sgr0)"
+}
+
+function echoRouge {
+  echo "$(tput setaf 9)$1$(tput sgr0)"
+}
+
+function echoJaune {
+  echo "$(tput setaf 11)$1$(tput sgr0)"
+}
+
+function checkTor {
+  echoOrange "Verifying Tor installation..."
+  if hash tor 2>/dev/null; then
+    echoVert "Tor is installed!"
+  else
+    echoOrange "Installing Tor..."
+    apt-get install tor
+    echoVert "Tor is now installed"
+  fi
+}
+
+echoOrange "Ensuring pip is up to date"
 sudo -H pip install --upgrade pip
+echoVert "Done."
 
-echo "\nInstalling stem\n"
+echoOrange "Installing stem"
 sudo -H pip install stem
+echoVert "Done."
 
-echo "\nInstalling pycurl\n"
+echoOrange "Installing pycurl"
 sudo -H pip install pycurl
+echoVert "Done."
+
+echoVert "You can now use : $(tput setaf 14)python countrify.py [desired country]$(tput sgr0)"
+echoVert "Try $(tput setaf 14)python countrify.py $(tput setaf 10)to display help message"
